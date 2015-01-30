24def1a2032edbc31b1cf9f342bf7954f14e0301
neilsen <neilsen>
2015-01-30 22:59:02 +0000

    start move to setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+import distutils
+from distutils.core import setup
+import glob
+
+import shlib 
+from shlib.build_shlib import SharedLibrary
+
+bin_files = glob.glob("bin/*.py") + glob.glob("bin/*.txt")
+
+libbpm = SharedLibrary(
+    'bpm',
+    sources = ['src/libbpm.c'],
+    include_dirs = ['include'],
+    extra_compile_args = ['-O3','-g','-Wall','-shared','-fPIC'])
+
+libfixcol = SharedLibrary(
+    'fixcol',
+    sources = ['src/libfixcol.c'],
+    include_dirs = ['include'],
+    extra_compile_args = ['-O3','-g','-Wall','-shared','-fPIC'])
+
+libmasksatr = SharedLibrary(
+    'masksatr',
+    sources = ['src/libmasksatr.c'],
+    include_dirs = ['include'],
+    extra_compile_args = ['-O3','-g','-Wall','-shared','-fPIC'])
+
+# The main call
+setup(name='pixcorrect',
+      version ='0.1.1',
+      license = "GPL",
+      description = "Pixel-level image correction",
+      author = "Eric Neilsen",
+      author_email = "neilsen@fnal.gov",
+      shlibs = [libbpm, libfixcol, libmasksatr],
+      packages = ['pixcorrect'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/pixcorrect.table'])],
+      )
+
