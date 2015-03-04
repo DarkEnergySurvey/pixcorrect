dfc7dbe442ed1fec45bff46d7f81bf229824c2d2
rgruendl <rgruendl>
2015-03-04 22:46:29 +0000

    Flat field normalization code... updated with expceptions and FLATMEDA/B keyword creation.

--- python/pixcorrect/normalize_flat.py
+++ python/pixcorrect/normalize_flat.py
@@ -12,6 +12,7 @@ from pixcorrect import proddir
 from pixcorrect.corr_util import logger, load_shlib
 from despyfits.DESImage import DESImage, DESImageCStruct, scan_fits_section, data_dtype
 from pixcorrect.PixCorrectDriver import PixCorrectImStep
+from pixcorrect import decaminfo
 
 # Which section of the config file to read for this step
 config_section = 'normflat'
@@ -27,14 +28,14 @@ class NormalizeFlat(PixCorrectImStep):
     step_name = config_section
 
     @classmethod
-    def __call__(cls, inlist, ccdnorm):
+    def __call__(cls, inlist, ccdnorm, ampborder):
         """Apply a flat field correction to an image
 
         :Parameters:
             - `inlist`: list of input and output flat DESImage(s) to normalize
             - `flat_im`:  the flat correction image to apply
 
-        Applies the correction "in place"
+        Applies the correction to each input and writes a separate output file.
         """
  
         logger.info('Initial Read of Flat Field Headers')
@@ -77,8 +78,7 @@ class NormalizeFlat(PixCorrectImStep):
                     try:
                         tmp_dict['scalmean']=float(temp_head['SCALMEAN'])
                     except:
-                        print("Image {:s} did not have a SCALMEAN keyword. Aborting!".format(tmp_dict['fname']))
-                        raise
+                        raise ValueError("Image %s did not have a SCALMEAN keyword. Aborting!" % tmp_dict['fname'])
 #
 #                   Finished first header census
 #                   Save file info and scalmean's to a list
@@ -91,9 +91,9 @@ class NormalizeFlat(PixCorrectImStep):
 #
 #           Input file was not present.           
 #
-            (type, value, trback)=sys.exc_info()
-            print("{:s} {:s} {:s} \n".format(inlist,type,value))
-            raise
+#            (type, value, trback)=sys.exc_info()
+#            print("{:s} {:s} {:s} \n".format(inlist,type,value))
+            raise IOError("File not found.  Missing input list %s " % inlist )
 #
 #       All information is now present. Determine the value that will be used in normalization.
 #
@@ -106,56 +106,35 @@ class NormalizeFlat(PixCorrectImStep):
                     if (tmp_rec['ccdnum']==ccdnorm):
                         print("Warning: More than one image with CCDNUM={:d} identified")
             if (normval is None):
-                print("CCDNUM: {:d} not among images read in list. Aborting.".format(ccdnorm))
-                raise
-            logger.info('Normaliztion set to CCD %d value: %.2f ' % (ccdnorm,normval))
+                raise ValueError("No image with CCDNUM=%d found among input list. Aborting!" % ccdnorm)
+            logger.info('Normaliztion: %.2f set based on value from CCD %d ' % (normval,ccdnorm))
         else:
             a_scalmean=np.array(scalmean_list)
             normval=np.median(a_scalmean)
-            logger.info('Normaliztion set to median value: %.2f ' % normval )
+            logger.info('Normaliztion: %.2f set based on median value of the ensemble ' % normval )
 #
 #       Go ahead and normalize the set
 #
         logger.info('Normalizing list')
         for tmp_record in norm_list:
+            logger.info('Working on image: %s ' % (tmp_record['fname']) )
             image=DESImage.load(tmp_record['fname'])
             nfactor=tmp_record['scalmean']/normval
             nfactor2=nfactor*nfactor
-            logger.info(' CCD: %2d, normalization factor: %.5f ' % (tmp_record['ccdnum'],nfactor) )
+            logger.info(' CCD: %2d, relative normalization factor: %.5f ' % (tmp_record['ccdnum'],nfactor) )
             image.data*=nfactor            
             image.weight*=nfactor2            
-
-            ampsecan = [x-1 for x in scan_fits_section(image, 'AMPSECA')]
-            ampsecbn = [x-1 for x in scan_fits_section(image, 'AMPSECB')]
-            if ((ampsecan[2]>ampsecan[3])or(ampsecbn[2]>ampsecbn[3])):
-                print("AMPSEC keyword detected with reversed order")
-            print "AMPSECA: ",ampsecan
-            print "AMPSECB: ",ampsecbn
-#           order x-ranges
-            if (ampsecan[0]>ampsecan[1]):
-                xtemp=ampsecan[0]
-                ampsecan[0]=ampsecan[1]
-                ampsecan[1]=xtemp
-            if (ampsecbn[0]>ampsecbn[1]):
-                xtemp=ampsecbn[0]
-                ampsecbn[0]=ampsecbn[1]
-                ampsecbn[1]=xtemp
-#           order y-ranges
-            if (ampsecan[2]>ampsecan[3]):
-                ytemp=ampsecan[2]
-                ampsecan[2]=ampsecan[3]
-                ampsecan[3]=ytemp
-            if (ampsecbn[2]>ampsecbn[3]):
-                ytemp=ampsecbn[2]
-                ampsecbn[2]=ampsecbn[3]
-                ampsecbn[3]=ytemp
-            print "AMPSECA(ordered): ",ampsecan
-            print "AMPSECB(ordered): ",ampsecbn
 #
-
-            image.data[ampsecan[2]:ampsecan[3]+1,ampsecan[0]:ampsecan[1]+1]*=image['GAINA']
-            image.data[ampsecbn[2]:ampsecbn[3]+1,ampsecbn[0]:ampsecbn[1]+1]*=image['GAINB']
-
+#           Create keywords that reflect the median value of the flat on each amp.
+#
+            for amp in decaminfo.amps:
+                datasecn=scan_fits_section(image,'DATASEC'+amp)
+                datasecn[0]=datasecn[0]+ampborder
+                datasecn[1]=datasecn[1]-ampborder
+                datasecn[2]=datasecn[2]+ampborder
+                datasecn[3]=datasecn[3]-ampborder
+                image['FLATMED'+amp]=np.median(image.data[datasecn[2]:datasecn[3]+1,datasecn[0]:datasecn[1]+1])
+            
             DESImage.save(image,tmp_record['oname'])
 
         logger.debug('Finished applying Flat')
@@ -176,10 +155,10 @@ class NormalizeFlat(PixCorrectImStep):
 
         flat_inlist = config.get(cls.step_name, 'inlist')
         ccdnorm = config.getint(cls.step_name, 'ccdnorm')
-#        logger.info('Reading flat correction from %s'% flat_fname)
-#        flat_im = DESImage.load(flat_fname)
-    
-        ret_code = cls.__call__(flat_inlist, ccdnorm)
+        ampborder = config.getint(cls.step_name, 'ampborder')
+
+        ret_code = cls.__call__(flat_inlist, ccdnorm, ampborder)
+
         return ret_code
 
     @classmethod
@@ -190,6 +169,8 @@ class NormalizeFlat(PixCorrectImStep):
                             help='List of input/output flat field image')
         parser.add_argument('--ccdnorm', type=int, default=-1,
                             help='Specific CCD to use for normalization (default=-1 --> use median over set)')
+        parser.add_argument('--ampborder', type=int, default=50,
+                            help='Length in pixels around periphery of each amp to ignore when calculating statistics (default=50)')
 
 normalize_flat = NormalizeFlat()
 
