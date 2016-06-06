cc91dc2c262f2102fe823c075edd56357d2ba526
Felipe Menanteau <felipe@illinois.edu>
2016-06-06 19:26:23 +0000

    Getting ready for version 0.3.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ libfpnumber = SharedLibrary(
 
 # The main call
 setup(name='pixcorrect',
-      version ='0.2.6',
+      version ='0.3.0',
       description = "Pixel-level image correction",
       author = "Eric Neilsen",
       author_email = "neilsen@fnal.gov",
