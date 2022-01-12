3d28e439ec82a5617fa085174a931e1e565a7048
Felipe Menanteau <felipe@illinois.edu>
2022-01-12 15:33:12 -0600

    Update for tag 3.0.9

--- python/pixcorrect/__init__.py
+++ python/pixcorrect/__init__.py
@@ -1,5 +1,5 @@
 __author__  = "Eric Neilsen, Felipe Menanteau"
-__version__ = '3.0.8'
+__version__ = '3.0.9'
 version = __version__
 from os import environ
 proddir = environ['PIXCORRECT_DIR']
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ libfpnumber = Extension(
 
 # The main call
 setup(name='pixcorrect',
-      version ='3.0.8',
+      version ='3.0.9',
       description = "Pixel-level image correction",
       author = "Eric Neilsen, Felipe Menanteau",
       author_email = "neilsen@fnal.gov",
