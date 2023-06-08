1d680d5e4b05965fa321a246e1831c0f9d3d4b05
Robert Gruendl <gruendl@illinois.edu>
2023-06-08 13:55:34 -0500

    Tagging as 3.1.7

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ libfpnumber = Extension(
 
 # The main call
 setup(name='pixcorrect',
-      version ='3.1.6',
+      version ='3.1.7',
       description = "Pixel-level image correction",
       author = "Eric Neilsen, Felipe Menanteau",
       author_email = "neilsen@fnal.gov",
